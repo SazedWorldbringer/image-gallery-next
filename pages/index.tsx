e216acb9d5c5720b9d0c757594f05917d0affd89
Atharva Pardeshi <atharva.exe@gmail.com>
2022-11-09 11:39:30 +0530

    feat: define type for images fetched from supabase, use images as prop with type Image in Gallery component, map images using BlurImage component, pass image as prop to BlurImage with type Image, update image fields with fetched data from supabase

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,25 +17,35 @@ export const getStaticProps = async () => {
   };
 };
 
-export default function Gallery() {
+type Image = {
+  id: number;
+  href: string;
+  imageSrc: string;
+  name: string;
+  username: string;
+};
+
+export default function Gallery({ images }: { images: Image[] }) {
   return (
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        <BlurImage />
+        {images.map((image) => (
+          <BlurImage key={image.id} image={image} />
+        ))}
       </div>
     </div>
   );
 }
 
-const BlurImage = () => {
+const BlurImage = ({ image }: { image: Image }) => {
   const [isLoading, setLoading] = useState(true);
 
   return (
-    <a href="#" className="group">
+    <a href={image.href} className="group">
       <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
         <Image
-          alt=""
-          src={"https://bit.ly/placeholder-img"}
+          alt={image.name}
+          src={image.imageSrc}
           layout="fill"
           objectFit="cover"
           className={`group-hover:opacity-75 duration-700 ease-in-out ${
@@ -46,8 +56,8 @@ const BlurImage = () => {
           onLoadingComplete={() => setLoading(false)}
         />
       </div>
-      <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">@leerob</p>
+      <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
     </a>
   );
 };
