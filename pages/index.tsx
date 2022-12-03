8f4f90421c08c072cb541d47c765d17dd5fcf166
Atharva Pardeshi <atharva.exe@gmail.com>
2022-12-03 08:38:02 +0530

    feat: update card ui, add noise background to gallery, add background animation to body

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,13 +27,14 @@ type Image = {
 
 export default function Gallery({ images }: { images: Image[] }) {
   return (
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+    <div className="bg-[url(https://grainy-gradients.vercel.app/noise.svg)] max-w-2xl my-10 mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-y-10 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <h1 className="self-center text-center col-start-2 row-start-2">Hello</h1>
         {images.map((image) => (
           <BlurImage key={image.id} image={image} />
         ))}
       </div>
-    </div>
+    </div >
   );
 }
 
@@ -42,22 +43,22 @@ const BlurImage = ({ image }: { image: Image }) => {
 
   return (
     <a href={image.href} className="group">
-      <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
+      <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg bg-gray-200 hover:scale-[0.95] duration-1000 ease-out">
         <Image
           alt={image.name}
           src={image.imageSrc}
           layout="fill"
           objectFit="cover"
-          className={`group-hover:opacity-75 duration-700 ease-in-out ${
-            isLoading
+          className={`grid group-hover:scale-150 duration-1000 ease-out 
+            ${isLoading
               ? "grayscale blur-2xl scale-110"
               : "grayscale-0 blur-0 scale-100"
-          }`}
+            }`}
           onLoadingComplete={() => setLoading(false)}
         />
       </div>
-      <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
+      <h3 className="mt-4 text-lg text-gray-700">{image.name}</h3>
+      <p className="mt-1 text-md font-medium text-gray-900">{'@' + image.username}</p>
     </a>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundSize: {
+        '400': '400%'
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': { backgroundPosition: "0% 50%" },
+          '50%': { backgroundPosition: "100% 50%" }
+        },
+      },
+      animation: {
+        gradient: 'gradient 10s ease infinite',
+      }
+    },
   },
   plugins: [require("@tailwindcss/aspect-ratio")],
 };
