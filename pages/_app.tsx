b7a9cf49c8046f426dc25432372b3df29d359afe
Atharva Pardeshi <atharva.exe@gmail.com>
2022-12-04 13:14:59 +0530

    fix: add title to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Layout from './layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <Layout title="Next Image Gallery">
       <Component {...pageProps} />
     </Layout>
   )
