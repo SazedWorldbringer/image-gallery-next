c674d0666cd8bd75b47e7e796f88f59ee80b6e0f
Atharva Pardeshi <atharva.exe@gmail.com>
2022-12-04 13:05:54 +0530

    feat: use Layout component, update some styles for index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/legacy/image";
 import { useState } from "react";
-import { createClient } from "@supabase/supabase-js";
 import Link from "next/link";
+import { createClient } from "@supabase/supabase-js";
 
 export const getStaticProps = async () => {
   const supabaseAdmin = createClient(
@@ -24,14 +24,15 @@ type Image = {
   imageSrc: string;
   name: string;
   username: string;
+  description: string;
 };
 
 export default function Gallery({ images }: { images: Image[] }) {
   return (
-    <div className="bg-[url(https://grainy-gradients.vercel.app/noise.svg)] max-w-2xl my-10 mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-y-10 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 wrapper">
+    <div className="bg-[url(https://grainy-gradients.vercel.app/noise.svg)] w-[100vw] max-w-2xl my-10 mx-auto py-16 px-4 sm:py-24 sm:px-6 md:max-w-3xl lg:max-w-6xl lg:px-9 xl:max-w-[90rem]">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-y-10 gap-x-6 md:grid-cols-3 lg:grid-cols-4 xl:gap-x-8 wrapper">
         <h1 className="self-center text-center">The Karaethon Cycle</h1>
-        {images?.map((image) => (
+        {images.map((image) => (
           <BlurImage key={image.id} image={image} />
         ))}
       </div>
@@ -44,7 +45,7 @@ const BlurImage = ({ image }: { image: Image }) => {
 
   return (
     <Link href={`images/${image.id}`} className="group">
-      <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg bg-gray-200 hover:scale-[0.95] duration-1000 ease-out">
+      <div className="aspect-w-5 aspect-h-4 md:aspect-w-4 md:aspect-h-5 lg:aspect-w-1 lg:aspect-h-1 xl:aspect-w-5 xl:aspect-h-4 overflow-hidden rounded-lg bg-gray-200 hover:scale-[0.95] duration-1000 ease-out">
         <Image
           alt={image.name}
           src={image.imageSrc}
