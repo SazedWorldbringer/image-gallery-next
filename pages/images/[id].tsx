7a583f39dd373b0afb8df0c31de804e1353214e1
Atharva Pardeshi <atharva.exe@gmail.com>
2022-12-04 13:17:18 +0530

    fix: export boilerplate from image page

--- pages/images/[id].tsx
+++ pages/images/[id].tsx
@@ -1,5 +1,13 @@
-/* import { createClient } from "@supabase/supabase-js";
 import Layout from "../layout";
+
+export const Page = () => {
+  return (
+    <Layout title='Image'>
+      Individual Image Page
+    </Layout>
+  )
+}
+/* import { createClient } from "@supabase/supabase-js";
 import { useState } from 'react'
 import Image from "next/image";
 
